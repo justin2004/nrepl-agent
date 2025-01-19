931832deb16ce4ec01a7b5f1731ef40d6451f0b8
Justin Dowdy <jdowdy@teamraft.com>
2025-01-19 13:44:13 -0600

    wip

--- src/main/java/NreplAgent.java
+++ src/main/java/NreplAgent.java
@@ -11,31 +11,13 @@ public class NreplAgent {
         if(port == null){
             port = "7888";
         }
-        ///
-        // IFn bob = Clojure.var("clojure.core", "+");
-        // IFn map = Clojure.var("clojure.core", "map");
-        // IFn reduce = Clojure.var("clojure.core", "reduce");
-        // IFn doall = Clojure.var("clojure.core", "doall");
-        // IFn vec = Clojure.var("clojure.core", "vec");
-        // bob.invoke(1,99);
-        // Clojure.read("clojure.set").getClass()
-        // vec.invoke(reduce.invoke(bob,Clojure.read("[1 3 5]")));
-        // (nrepl.server/start-server :port port
-        //                            :handler (iced.nrepl/wrap-iced (nrepl.server/default-handler)))
-        ///
         IFn require = Clojure.var("clojure.core", "require");
         require.invoke(Clojure.read("nrepl.server"));
         require.invoke(Clojure.read("iced.nrepl"));
         IFn wrapIced = Clojure.var("iced.nrepl", "wrap-iced");
-        // IFn handler = Clojure.var("nrepl.middleware", "wrap-repl");
-        // Object icedMiddleware = wrapIced.invoke(handler);
         IFn defaultHandler = Clojure.var("nrepl.server", "default-handler");
         IFn start = Clojure.var("nrepl.server", "start-server");
-        // int port = 7888;
-        // start.invoke(Clojure.read(":port"), Clojure.read(Integer.toString(port)));
-        // start.invoke(Clojure.read(":port"), Clojure.read(Integer.toString(port)), Clojure.read(":handler"), icedMiddleware);
-        // start.invoke(Clojure.read(":port"), Clojure.read(Integer.toString(port)), Clojure.read(":handler"), wrapIced.invoke(defaultHandler.invoke()));
         start.invoke(Clojure.read(":port"), Clojure.read(port), Clojure.read(":handler"), wrapIced.invoke(defaultHandler.invoke()));
         System.out.println("nrepl server started on port " + port);
-}
+    }
 }
